5c3fd7f87f6b8af6909640a17e0af0e03320423c
YingVickyCao <ying.vicky.cao@outlook.com>
2021-07-02 08:46:18 +0800

    选择结构

---
+++ test-Object-C/test-Object-C/_6_condition/_6_condition.m
@@ -0,0 +1,79 @@
+//
+//  _6_condition.m
+//  test-Object-C
+//
+//  Created by hades on 2021/7/2.
+//  Copyright © 2021 hades. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char* argv[]){
+    @autoreleasepool {
+        // if
+        {
+            int num = 5;
+            if (num > 0 ) {
+                num -= -2;
+                NSLog(@"%i",num);   // 7
+            }
+        }
+        
+        // if - else
+        {
+            int intVar = 15;
+            if (intVar % 2 == 0) {
+                NSLog(@"The number is even");
+            }
+            else{
+                NSLog(@"The number is odd");
+            }
+        }
+        
+        // 复合条件测试
+        {
+            int grade = 78;
+            int count_gradle_70_to_79 = 0;
+            if (grade >= 70 && grade <=79) {
+                count_gradle_70_to_79++;
+            }
+            NSLog(@"count 70<=gradle<=79 is %i",count_gradle_70_to_79);
+            
+            int index = 8;
+            if (index < 0 || index > 99) {
+                NSLog(@"Error:index is out of range");
+            }
+        }
+        
+        // 嵌套if
+        {
+            bool dog = true;
+            bool big = false;
+            if (dog) {
+                if (big) {
+                    NSLog(@"Big dog");
+                }
+                else{
+                    
+                }
+                NSLog(@"Little dog");
+            }
+        }
+        
+        // else if
+        {
+            int num = 5;
+            int sign = -1;
+            if (num < 0) {
+                sign = -1;
+            }
+            else if(num == 0){
+                sign = 0;
+            }else{
+                sign = 1;
+            }
+            NSLog(@"sign is %i",sign);
+        }
+    }
+    return 0;
+}
