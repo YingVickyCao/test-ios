a0155230f8aace6893f725274daf9566832b9e9b
YingVickyCao <ying.vicky.cao@outlook.com>
2022-07-16 22:13:01 +0800

    swift

--- Test-Swift/Test-Swift/main.swift
+++ Test-Swift/Test-Swift/main.swift
@@ -1,11 +1,3 @@
-//
-//  main.swift
-//  Test-Swift
-//
-//  Created by hades on 2022/7/16.
-//
-
 import Foundation
 
 print("Hello, World!")
-
---
+++ Test-Swift/Test-Swift/test.m
@@ -0,0 +1,8 @@
+#import <Foundation/Foundation.h>
+
+int main(int argc,const char* argv[]){
+    @autoreleasepool {
+        int c = sizeof(int);
+        printf("%d", c);
+    }
+}
